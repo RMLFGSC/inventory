f3b4389c8ccebbf5bcb8cbb0ad8920226df596a7
wana16 <joannaducut2@gmail.com>
2025-02-26 11:27:24 +0800

    wadsd

--- includes/navbar_mmo.php
+++ includes/navbar_mmo.php
@@ -5,7 +5,7 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
-                <a class="nav-link" href="../admin/index.php">
+                <a class="nav-link" href="../superusermmo/index.php">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
@@ -60,10 +60,22 @@
          
 
             <li class="nav-item">
-                <a class="nav-link" href="items.php">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#auth4"
+                    aria-expanded="true" aria-controls="collapseUtilities">
                     <i class="fas fa-fw fa-archive"></i>
-                    <span>Reports</span></a>
+                    <span>Reports</span>
+                </a>
+                <div id="auth4" class="collapse" aria-labelledby="headingUtilities"
+                    data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <a class="collapse-item" href="reports-stockin.php">Stock-in</a>
+                        <a class="collapse-item" href="reports-requisition.php">Requisition</a>
+                        <a class="collapse-item" href="reports-issuance.php">Issuance</a>
+                </div>
             </li>
+
+
+        
 
             <!-- Divider -->
             <hr class="sidebar-divider d-none d-md-block">
